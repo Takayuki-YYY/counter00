bdce477b8f1addd7099fcba18689652da845138b
Takayuki-YYY <81605210+Takayuki-YYY@users.noreply.github.com>
2021-04-26 10:38:06 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     counter = 0
 })
-input.onButtonPressed(Button.B, function () {
+input.onGesture(Gesture.Shake, function () {
     counter += 1
 })
 let counter = 0
